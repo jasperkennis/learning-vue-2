e41b4c6286620f5377b112c41b27f1a7b26ea8c4
Jasper Kennis <contact@jasperkennis.nl>
2018-07-22 23:19:57 +0200

    created browsing page for each type and linked

--- build-a-bot/src/router/index.js
+++ build-a-bot/src/router/index.js
@@ -1,5 +1,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import BrowseParts from '../parts/BrowseParts.vue';
+import RobotHeads from '../parts/RobotHeads.vue';
+import RobotArms from '../parts/RobotArms.vue';
+import RobotTorsos from '../parts/RobotTorsos.vue';
+import RobotBases from '../parts/RobotBases.vue';
 import HomePage from '../home/HomePage.vue';
 import PartInfo from '../parts/PartInfo.vue';
 import RobotBuilder from '../build/RobotBuilder.vue';
@@ -15,6 +20,32 @@ export default new Router({
     component: RobotBuilder,
     name: 'Build',
     path: '/build',
+  }, {
+    children: [
+      {
+        component: RobotHeads,
+        name: 'BrowseHeads',
+        path: 'heads',
+      },
+      {
+        component: RobotArms,
+        name: 'BrowseArms',
+        path: 'arms',
+      },
+      {
+        component: RobotTorsos,
+        name: 'BrowseTorsos',
+        path: 'torsos',
+      },
+      {
+        component: RobotBases,
+        name: 'BrowseBases',
+        path: 'bases',
+      },
+    ],
+    component: BrowseParts,
+    name: 'BrowsParts',
+    path: '/parts/browse',
   }, {
     component: PartInfo,
     name: 'Parts',
