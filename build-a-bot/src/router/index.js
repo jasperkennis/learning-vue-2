308a4cdbd8abab1c799381ef00655f0621be8af9
Jasper Kennis <contact@jasperkennis.nl>
2018-07-22 17:44:05 +0200

    defined some routs

--- build-a-bot/src/router/index.js
+++ build-a-bot/src/router/index.js
@@ -1,8 +1,18 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import HomePage from '../home/HomePage.vue';
+import RobotBuilder from '../build/RobotBuilder.vue';
 
 Vue.use(Router);
 
 export default new Router({
-  routes: [],
+  routes: [{
+    component: HomePage,
+    name: 'Home',
+    path: '/',
+  }, {
+    component: RobotBuilder,
+    name: 'RobotBuilder',
+    path: '/build',
+  }],
 });
