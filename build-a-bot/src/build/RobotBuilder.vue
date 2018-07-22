8b182563e70bbe7dc2aabe4487fc053342d919e3
Jasper Kennis <contact@jasperkennis.nl>
2018-07-22 12:00:29 +0200

    bind images to parts and also a first action

--- build-a-bot/src/build/RobotBuilder.vue
+++ build-a-bot/src/build/RobotBuilder.vue
@@ -2,31 +2,31 @@
     <div>
     <div class="top-row">
       <div class="top part">
-        <img src="./images/head-big-eye.png" title="head"/>
+        <img v-bind:src="availableParts.heads[0].src" title="head"/>
         <button class="prev-selector">&#9668;</button>
-        <button class="next-selector">&#9658;</button>
+        <button v-on:click="selectNextHead()" class="next-selector">&#9658;</button>
       </div>
     </div>
     <div class="middle-row">
       <div class="left part">
-        <img src="./images/arm-articulated-claw.png" title="left arm"/>
+        <img v-bind:src="availableParts.arms[0].src" title="left arm"/>
         <button class="prev-selector">&#9650;</button>
         <button class="next-selector">&#9660;</button>
       </div>
       <div class="center part">
-        <img src="./images/torso-flexible-gauged.png" title="left arm"/>
+        <img  v-bind:src="availableParts.torsos[0].src" title="left arm"/>
         <button class="prev-selector">&#9668;</button>
         <button class="next-selector">&#9658;</button>
       </div>
       <div class="right part">
-        <img src="./images/arm-dual-claw.png" title="left arm"/>
+        <img  v-bind:src="availableParts.arms[0].src" title="left arm"/>
         <button class="prev-selector">&#9650;</button>
         <button class="next-selector">&#9660;</button>
       </div>
     </div>
     <div class="bottom-row">
       <div class="bottom part">
-        <img src="./images/base-single-wheel.png" title="left arm"/>
+        <img  v-bind:src="availableParts.bases[0].src" title="left arm"/>
         <button class="prev-selector">&#9668;</button>
         <button class="next-selector">&#9658;</button>
       </div>
@@ -35,10 +35,19 @@
 </template>
 
 <script>
+import availableParts from '../data/parts';
+
 export default {
   name: 'RobotBuilder',
   data() {
-    return {};
+    return {
+      availableParts,
+    };
+  },
+  methods: {
+    selectNextHead() {
+      console.log('Select next head called');
+    },
   },
 };
 </script>
