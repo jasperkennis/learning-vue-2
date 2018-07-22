9d2d82e31371d4dc8ceef0041d09764135dfd7f3
Jasper Kennis <contact@jasperkennis.nl>
2018-07-22 10:18:47 +0200

    added some elements for the header

--- build-a-bot/src/App.vue
+++ build-a-bot/src/App.vue
@@ -1,5 +1,15 @@
 <template>
   <div id="app">
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item">
+            <img class="logo" src="./assets/build-a-bot-logo.png">
+            Build a bot
+          </li>
+        </ul>
+      </nav>
+    </header>
     <main>
       <HomePage msg="Welcome to Your Vue.js App"/>
     </main>
@@ -32,5 +42,28 @@ main {
   margin: 0 auto;
   padding: 30px;
   width: 1024px;
+}
+
+header {
+  background-color: #999;
+  margin: 0 auto;
+  width: 1084px;
+}
+
+ul {
+  display: flex;
+  padding: 3px;
+}
+
+.nav-item {
+  border-right: 1px solid #bbb;
+  display: inline-block;
+  font-size: 22px;
+  padding: 5px 10px;
+}
+
+.logo {
+  height: 30px;
+  vertical-align: middle;
 }
 </style>
--- build-a-bot/src/components/HomePage.vue
+++ build-a-bot/src/components/HomePage.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="home">
+    <div>
+      <img class="robot" src="../assets/robot-home.png"  aria-hidden="true" />
+    </div>
+    <div class="get-started">
+      <a href="">Get started</a> building your first robot!
+    </div>
   </div>
 </template>
 
@@ -14,18 +20,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.home {
+  text-align: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.robot {
+  height: 300px;
 }
 </style>
